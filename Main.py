e87d5586281d3fdf2204b75541c7ca4b08bea213
Gautier Julien <jululien.gautier@laposte.net>
2019-03-20 11:26:20 +0100

    push pour prendre

--- Main.py
+++ Main.py
@@ -1,6 +1,16 @@
 from Classes import Drogues
 
+# coding: utf-8
 
-ketamine = Drogues.Drogues("ketamine",2.5)
+import sqlite3
+
+# connection
+conn = sqlite3.connect('genieketamine.db')
+cursor = conn.cursor()
+
+# Insertion des données
+data = {"name": "olivier", "age": 30}
+cursor.execute("""INSERT INTO users(name, age) VALUES(:name, :age)""", data)
+ketamine = Drogues.Drogues("ketamine", 2.5)
 
 print(ketamine)
