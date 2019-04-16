aaa4f029bb37e824f30c4a7e9505353ee45f9483
Biggy <jululien.gautier@laposte.net>
2019-04-16 14:58:59 +0200

    deploiement?

--- Main.py
+++ Main.py
@@ -3,7 +3,10 @@ from Classes import Drogues
 # coding: utf-8
 
 import sqlite3
+import os
 
+# var d'env'
+qualifier = os.environ['QUALIFIER']
 # connection
 conn = sqlite3.connect('GenieKetamine')
 cursor = conn.cursor()
